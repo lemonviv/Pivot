0cf276e9707ce4c900a13026f50ad1272a3d2821
Wu Yuncheng <wuyuncheng@r-194-121-25-172.comp.nus.edu.sg>
2019-10-10 11:25:35 +0800

    initialization from local

---
+++ cmake-build-debug/CMakeFiles/CollaborativeML.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CollaborativeML.dir/main.cpp.o"
+  "CollaborativeML.pdb"
+  "CollaborativeML"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CollaborativeML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CollaborativeML.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CollaborativeML.
+# This may be replaced when dependencies are built.
